aaab6d55416bd309bf6d70a95a75c1e7c6a343a1
Michael Peter <michael_piterct@hotmail.com>
2021-06-12 00:17:01 -0300

    Inform competition number

---
+++ Bots/Bot.MegaSena/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bot.MegaSena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inform competition number: ");
+
+            string competitionNumber = Console.ReadLine();
+
+            Console.WriteLine(competitionNumber);
+
+            Console.ReadKey();
+        }
+    }
+}
