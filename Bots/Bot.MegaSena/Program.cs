313a348f9190c7d019af338688bdd0b4c1071a19
Michael Peter <michael_piterct@hotmail.com>
2021-06-12 02:57:57 -0300

    Final Numbers Mega Sena

--- Bots/Bot.MegaSena/Program.cs
+++ Bots/Bot.MegaSena/Program.cs
@@ -1,4 +1,5 @@
 ﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net;
@@ -11,10 +12,9 @@ namespace Bot.MegaSena
         {
             Console.WriteLine("Add competition number: ");
             string competitionNUmber = Console.ReadLine();
+            string finalNumbers = string.Empty;
 
-           
-
-            string url = string.Format("http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbwMPI0sDBxNXAOMwrzCjA0sjIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wNnUwNHfxcnSwBgIDUyhCvA5EawAjxsKckMjDDI9FQE-F4ca/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0KO6H80AU71KG7J0072/res/id=buscaResultado/c=cacheLevelPage//p=concurso={0}?timestampAjax=1623475384093",competitionNUmber);
+            string url = string.Format("http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbwMPI0sDBxNXAOMwrzCjA0sjIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wNnUwNHfxcnSwBgIDUyhCvA5EawAjxsKckMjDDI9FQE-F4ca/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0KO6H80AU71KG7J0072/res/id=buscaResultado/c=cacheLevelPage//p=concurso={0}?timestampAjax=1623475384093", competitionNUmber);
             string json;
 
             using (WebClient client = new WebClient())
@@ -22,6 +22,17 @@ namespace Bot.MegaSena
                 client.Headers["Cookie"] = "security=true";
                 json = client.DownloadString(url);
             }
+
+            var competitionResult = JsonConvert.DeserializeObject<Game>(json);
+
+            foreach (var number in competitionResult.ListaDezenas)
+            {
+                string processedNumber = number.Length > 2 ? number.Substring(1, 2) : number;
+                finalNumbers += string.Concat(processedNumber, " ");
+            }
+
+            Console.WriteLine("-------------------------------------------------------------------");
+            Console.WriteLine(string.Format("Result: {0}", finalNumbers));
 
             Console.ReadKey();
         }
