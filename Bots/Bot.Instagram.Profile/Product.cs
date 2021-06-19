1bf09e3162112813ab5ea504c32ed00002aaa27a
Michael Peter <michael_piterct@hotmail.com>
2021-06-19 17:53:07 -0300

    get image url and website

--- Bots/Bot.Instagram.Profile/Product.cs
+++ Bots/Bot.Instagram.Profile/Product.cs
@@ -7,16 +7,9 @@
             NameProduct = nameProduct;
         }   
         public string NameProduct { get; set; }
-        public string IosAppName { get; set; }
-        public string IosAppId { get; set; }
-        public string IosUrl { get; set; }
-        public string AndroidAppName { get; set; }
-        public string AndroidAppId { get; set; }
-        public string AndroidUrl { get; set; }
-        public string Type { get; set; }
-        public string Image { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
-        public string Url { get; set; }
+        public string imageUrl { get; set; }
+        public string urlProductWebSite { get; set; }
+
     }
 }
--- Bots/Bot.Instagram.Profile/ZoomProduct.cs
+++ Bots/Bot.Instagram.Profile/ZoomProduct.cs
@@ -13,7 +13,9 @@ namespace Bot.Zoom
         public async static ValueTask<Product> GetProduct(string productName)
         {
             var product = new Product(productName);
-            string url = @"https://www.zoom.com.br/search?q=" + productName + "";
+            string rooUrl = "https://www.zoom.com.br";
+            string searchUrl = @"/search?q=" + productName + "";
+            string url = string.Format("{0}{1}", rooUrl, searchUrl);
             string markup;
 
             using (WebClient client = new WebClient())
@@ -21,23 +23,24 @@ namespace Bot.Zoom
                 markup = client.DownloadString(url);
             }
 
-           HtmlDocument html = new HtmlDocument();
-           html.LoadHtml(markup);
+            HtmlDocument html = new HtmlDocument();
+            html.LoadHtml(markup);
 
             var divs = html.DocumentNode.Descendants("div")
                 .Where(node => node.GetAttributeValue("class", "").Equals("SearchPage_SearchList__HL7RI col")).ToList();
 
-            foreach(var div in divs)
+            foreach (var div in divs)
             {
-               // var divProducts = div.Descendants("div").ToList();
+                // var divProducts = div.Descendants("div").ToList();
 
                 var divProducts = div.Descendants("div")
                     .Where(node => node.GetAttributeValue("class", "").Equals("card card--prod")).ToList();
 
                 foreach (var divProduct in divProducts)
                 {
-                    var cardBody = div.Descendants("a").FirstOrDefault().ChildAttributes("href").FirstOrDefault().Value;
-                    var imageUrl = div.Descendants("img").FirstOrDefault().ChildAttributes("src").FirstOrDefault().Value;
+                    product.urlProductWebSite = string.Format("{0}{1}", rooUrl, divProduct.Descendants("a").FirstOrDefault().ChildAttributes("href").FirstOrDefault().Value);
+                    product.imageUrl = divProduct.Descendants("img").FirstOrDefault().ChildAttributes("src").FirstOrDefault().Value;
+
                     var title = div.Descendants("img").FirstOrDefault().ChildAttributes("title").FirstOrDefault().Value;
                 }
             }
