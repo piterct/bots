580d6974322fd7d943f4a56e3f654cbdc90698d2
Michael Peter <michael_piterct@hotmail.com>
2021-06-19 13:13:24 -0300

    profile class

---
+++ Bots/Bot.Instagram.Profile/Instagram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Instagram.Profile
+{
+    public static class Instagram
+    {
+        public static Profile GetProfileByUser(string username)
+        {
+
+        }
+    }
+}
