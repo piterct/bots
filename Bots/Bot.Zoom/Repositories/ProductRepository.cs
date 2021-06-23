db23bb26380f5c60ef2d38992adc62510fed460a
Michael Peter <michael_piterct@hotmail.com>
2021-06-22 23:07:13 -0300

    IProductRepository

--- Bots/Bot.Zoom/Repositories/ProductRepository.cs
+++ Bots/Bot.Zoom/Repositories/ProductRepository.cs
@@ -4,6 +4,10 @@ namespace Bot.Zoom.Repositories
 {
     public class ProductRepository : IProductRepository
     {
-
+        private readonly IBotZoomSettings _botZoomSettings;
+        public ProductRepository(IBotZoomSettings botZoomSettings)
+        {
+            _botZoomSettings = botZoomSettings;
+        }
     }
 }
