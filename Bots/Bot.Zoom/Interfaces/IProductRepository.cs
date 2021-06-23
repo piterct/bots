69b8824697e2db2def4f025f45243b5683c4a54a
Michael Peter <michael_piterct@hotmail.com>
2021-06-22 23:16:00 -0300

    interface InsertProducts

--- Bots/Bot.Zoom/Interfaces/IProductRepository.cs
+++ Bots/Bot.Zoom/Interfaces/IProductRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Bot.Zoom.Interfaces
+﻿using Bot.Zoom.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Bot.Zoom.Interfaces
 {
     public interface IProductRepository
     {
+        ValueTask<bool> InsertProducts(List<Product> products);
     }
 }
