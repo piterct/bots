f766401e010ac3d3d821cfed89de0c4483928be5
Michael Peter <michael_piterct@hotmail.com>
2021-06-22 23:12:15 -0300

    InsertProducts

--- Bots/Bot.Zoom/Repositories/ProductRepository.cs
+++ Bots/Bot.Zoom/Repositories/ProductRepository.cs
@@ -1,4 +1,9 @@
 ﻿using Bot.Zoom.Interfaces;
+using Bot.Zoom.Models;
+using Dapper;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
 
 namespace Bot.Zoom.Repositories
 {
@@ -8,6 +13,26 @@ namespace Bot.Zoom.Repositories
         public ProductRepository(IBotZoomSettings botZoomSettings)
         {
             _botZoomSettings = botZoomSettings;
+        }
+
+        public async ValueTask<bool> InsertProducts(List<Product> products)
+        {
+            int rows = 0;
+
+            using (var connection = new SqlConnection(_botZoomSettings.ConnectionString))
+            {
+                foreach (var product in products)
+                {
+
+                    rows = await connection.ExecuteAsync("INSERT INTO Tabela(Name, Cpf, Password, Email, Mobile, IsEnabled, UserTypeId, CreatedAt) " +
+                                                         "VALUES(@CompanyDocument, @ClientDocument, @ClientName, @ClientEmail, @ClientPhone, @CreatedAt, @Message )",
+                           new { ticket.CompanyDocument, ticket.ClientDocument, ticket.ClientName, ticket.ClientEmail, ticket.ClientPhone, ticket.CreatedAt, ticket.Message }, transaction);
+                }
+
+            }
+
+
+            return rows > 0;
         }
     }
 }
